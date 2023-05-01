78ccea66a4e9adc66b775a8e8ee4c6aa5c9656f2
roki533 <87115154+roki533@users.noreply.github.com>
2023-05-01 15:02:22 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def communicate():
 
 
 # ユーザーインターフェイスの構築
-st.title("BSI Support desk")
+st.title("Chat2")
 st.write("ChatGPT APIを使ったチャットボットです。")
 
 user_input = st.text_input("メッセージを入力してください。", key="user_input", on_change=communicate)
